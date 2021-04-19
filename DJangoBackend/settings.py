5fcaefcdba43813a81ea4df825ca47cf3c37f70e
trivium159 <angelodm2@msn.com>
2021-04-19 16:22:09 -0400

    Update settings.py

--- DJangoBackend/settings.py
+++ DJangoBackend/settings.py
@@ -117,6 +117,9 @@ STATICFILES_DIRS = (
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+#  Add configuration for static files storage using whitenoise
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
 from neomodel import config 
 config.DATABASE_URL = 'bolt://neo4j:stories123@localhost:7687'  # for local usage | Password = stories123
 
