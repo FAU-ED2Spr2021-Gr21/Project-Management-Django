c682ee06abec927c213e6333da50270391dcc471
trivium159 <angelodm2@msn.com>
2021-04-19 16:11:19 -0400

    Update settings.py

--- DJangoBackend/settings.py
+++ DJangoBackend/settings.py
@@ -111,14 +111,17 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
-
+PROJECT_ROOT   =   os.path.join(os.path.abspath(__file__))
+STATIC_ROOT  =   os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 
+# Extra lookup directories for collectstatic to find static files
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static"),
+    os.path.join(PROJECT_ROOT, 'static'),
 )
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+#  Add configuration for static files storage using whitenoise
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 from neomodel import config 
 config.DATABASE_URL = 'bolt://neo4j:stories123@localhost:7687'  # for local usage | Password = stories123
