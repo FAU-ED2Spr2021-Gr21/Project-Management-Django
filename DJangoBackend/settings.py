06065f0630dc932aae2717d16e8cd9b55b2435dd
trivium159 <angelodm2@msn.com>
2021-04-19 20:47:07 -0400

    Update settings.py

--- DJangoBackend/settings.py
+++ DJangoBackend/settings.py
@@ -9,6 +9,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 import os
 from pathlib import Path
+from neomodel import config
+from .env import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -116,9 +118,8 @@ STATICFILES_DIRS = (
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-
-from neomodel import config 
-config.DATABASE_URL = 'bolt://neo4j:stories123@192.168.50.227:7687'  # for local usage | Password = stories123
+ 
+config.DATABASE_URL = env('DATABASE_URL')  # for local usage | Password = stories123
 
 # Rest-Framework settings
 REST_FRAMEWORK = {
