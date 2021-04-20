f123d040ff100d969924d71216b1a56eff03f08e
Micbro-fau <micahbrown2016@fau.edu>
2021-04-20 02:56:37 -0400

    Frontend Updates & Picture Updates

--- fetch_api/utils.py
+++ fetch_api/utils.py
@@ -96,8 +96,8 @@ def fetch_comparisons(node_info):
     where s1 <> s2
     and s1.name = "''' + name + '''" with  s1 as s1, s2 as s2, count(distinct x) as shared, collect(distinct x.text) as stuff
     order by shared desc
-    return collect(s2.name)[0], stuff'''
-
+    return collect(s2.name)'''
+    
     comparisons = db.cypher_query(comparison)[0]
     return comparisons
 
